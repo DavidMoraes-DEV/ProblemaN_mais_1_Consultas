4aa88060cf3435439ad5ba935d1b7ef5792e5cee
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-04 18:49:15 -0300

    Ponto e explicacao da onde eh gerado o problemas das N mais 1 Consultas

--- backend/src/main/java/com/example/demo/dto/ProductDTO.java
+++ backend/src/main/java/com/example/demo/dto/ProductDTO.java
@@ -26,6 +26,18 @@ public class ProductDTO {
 	public ProductDTO(Product product) {
 		id = product.getId();
 		name = product.getName();
+		/*AQUI que é gerado o problema das N+1 Consuntas, pois se remover esse acesso do DTO para a lista de categorias de cada produto
+		 	- o Hibernate Não irá fazer aquelas N consultas para vincular as categorias de cada produto e apenas executará o comando inicial buscando os produtos
+				- Porém as categorias irá retornar VAZIA
+			- Então podemos dizer que se não acessarmos as categorias de cada produto
+				- Não serão feitas as N consultas para buscar as categorias de cada produto buscado na primeira consulta
+			- O JPA por padrão:
+				- Quando se tem um objeto e associado a ele tem vários outros objetos
+					- Esses vários outros objeto por padrão não são carregados
+						- Isso é chamado de Lazy Loading
+							- O que é Lazy Loading: Não será carregados os objetos associados. Apenas será carregado se PRECISAR
+							- Nesse exemplo precisa e por isso foi colocado a consulta abaixo, porém isso gera o problema das N+1 Consultas
+								- Pois o JPA tem que voltar no banco para buscar as categorias de cada produto da primeira consulta*/
 		categories = product.getCategories().stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
--- backend/src/main/java/com/example/demo/service/ProductService.java
+++ backend/src/main/java/com/example/demo/service/ProductService.java
@@ -15,7 +15,19 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
-
+	
+	/*
+	 * Um DTO é importante de se utilizar porque:
+	 	- Como o DTO é um objeto pra transferir dados e com ele podemos:
+	 		- Tranferir apenas os dados que desejamos
+	 		- É retirado o monitoramento dos dados pela JPA
+	 			- Ou seja um DTO não é monitorado
+	 			- E é independente da conexão com o banco de dados
+	 			- Isso segnifica que ao passar para o DTO:
+	 				- Não se tem mais o compromisso de mexer com banco de dados
+	 				- E transações com o banco, pois as transações estão encerradas
+	 		- Tudo isso é uma BOA PRÁTICA		
+	*/
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> find(PageRequest pageRequest) {
 		Page<Product> list = repository.findAll(pageRequest);
